0b43d9e7985e9cb3b1dd03d37e1c73714fac318f
Ranhao Kang <ranhao@momenta.ai>
2018-04-18 20:53:34 +0800

    update

--- main.py
+++ main.py
@@ -104,6 +104,7 @@ def train(args):
     if not args.no_cuda:
         model = model.cuda()
         model.optical_flow_estimators = [i.cuda() for i in model.optical_flow_estimators]
+        model.context_networks = [i.cuda() for i in model.context_networks]
     model.train()
 
     # TODO: change optimizer to S_long & S_fine (same as flownet2)
--- model.py
+++ model.py
@@ -41,7 +41,7 @@ class Net(nn.Module):
 
             flow = self.optical_flow_estimators[l](src_features[l], cost_volume, flow)
             # use context to refine
-            flow_refined = self.context_networks(src_features[l], flow)
+            flow_refined = self.context_networks[l](src_features[l], flow)
             
             flow_pyramid.append(flow)
             flow_refined_pyramid.append(flow_refined)
