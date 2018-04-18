a8044cdfbba3889fafd32739f0c7a5c4b41922b3
Ranhao Kang <ranhao@momenta.ai>
2018-04-18 19:54:23 +0800

    .

--- modules.py
+++ modules.py
@@ -41,7 +41,7 @@ class CostVolumeLayer(nn.Module):
         H, W = src.size()[2:]
         import time
         t_start = time.time()
-        if src.size(1) >= args.search_range*2+1)**2:
+        if src.size(1) >= (args.search_range*2+1)**2:
             output = torch.zeros_like(src)[:,:args.search_range*2+1)**2,:,:]
         else:
             output = F.pad(torch.zeros_like(src), ((args.search_range*2+1)**2 - src.size(1)),0,0,0,0,0))
