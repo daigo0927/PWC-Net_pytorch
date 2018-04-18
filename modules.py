e3b6303829a12cb9c372f09ec40d39b4bc49dfe5
Ranhao Kang <ranhao@momenta.ai>
2018-04-18 18:53:59 +0800

    .

--- modules.py
+++ modules.py
@@ -22,6 +22,8 @@ class WarpingLayer(nn.Module):
 
         grid = Variable(data = grid, volatile = not self.training)
 
+        print(x.size(), grid.size(), flow.size())
+
         # variableFlow = torch.cat([ variableFlow[:, 0:1, :, :] / ((variableInput.size(3) - 1.0) / 2.0), variableFlow[:, 1:2, :, :] / ((variableInput.size(2) - 1.0) / 2.0) ], 1)
         return F.grid_sample(x, grid + flow)
 
