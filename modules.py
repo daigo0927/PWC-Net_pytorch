a0cd20d3216cecd7a3373067af619bab52dbad10
Ranhao Kang <ranhao@momenta.ai>
2018-04-20 20:05:40 +0800

    .

--- modules.py
+++ modules.py
@@ -102,11 +102,11 @@ class FeaturePyramidExtractor(nn.Module):
         # so here uses setattr to add levels
         self.levels = []
         for l in range(args.num_levels):
-            layer = nn.Sequential(
+            layer = nn.DataParallel(nn.Sequential(
                 nn.Conv2d(in_channels = 3 if l == 0 else args.lv_chs[l-1], out_channels = args.lv_chs[l], kernel_size = 3, stride = 2, padding = 1, dilation = 1, groups = 1, bias = True),
                 nn.LeakyReLU(inplace = True),
                 nn.Conv2d(in_channels = args.lv_chs[l], out_channels = args.lv_chs[l], kernel_size = 3, stride = 1, padding = 1, dilation = 1, groups = 1, bias = True),
-                nn.LeakyReLU(inplace = True)).cuda()
+                nn.LeakyReLU(inplace = True)))
 
             setattr(self, f'level{l+1}', layer)
             self.levels.append(layer)
