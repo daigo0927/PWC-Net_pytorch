95afb19e324460dc3594f199a8f37df4d7842464
Ranhao Kang <ranhao@momenta.ai>
2018-04-20 19:28:47 +0800

    .

--- modules.py
+++ modules.py
@@ -13,8 +13,11 @@ class WarpingLayer(nn.Module):
     def forward(self, x, flow):
         args = self.args
         # build coord matrix
+
+        torch.set_default_tensor_type('torch.FloatTensor')
         torchHorizontal = torch.linspace(-1.0, 1.0, x.size(3), out = torch.FloatTensor).view(1, 1, 1, x.size(3)).expand(x.size(0), 1, x.size(2), x.size(3))
         torchVertical = torch.linspace(-1.0, 1.0, x.size(2), out = torch.FloatTensor).view(1, 1, x.size(2), 1).expand(x.size(0), 1, x.size(2), x.size(3))
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
         if not args.no_cuda:
             torchHorizontal = torchHorizontal.cuda()
             torchVertical = torchVertical.cuda()
