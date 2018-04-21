bf9981cc651d1082c2dc6ab146c787a483eb7387
Ranhao Kang <ranhao@momenta.ai>
2018-04-21 14:33:10 +0800

    add resize

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ import imageio
 import torch
 import random
 import cv2
+from functools import partial
 from flow_utils import load_flow
 from abc import abstractmethod, ABCMeta
 
@@ -55,10 +56,19 @@ class BaseDataset(Dataset, metaclass = ABCMeta):
             img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)[:,:,np.newaxis]
 
         images = [img1, img2]
-        if self.shape is not None:
-            cropper = StaticRandomCrop(img1.shape[:2], self.shape)
+        if self.crop_shape is not None:
+            cropper = StaticRandomCrop(img1.shape[:2], self.crop_shape)
             images = list(map(cropper, images))
             flow = cropper(flow)
+        if self.resize_shape is not None:
+            resizer = partial(cv2.resize, dsize = (0,0), dst = self.resize_shape)
+            images = list(map(resizer, images))
+            flow = resizer(flow)
+        elif self.resize_scale is not None:
+            resizer = partial(cv2.resize, dsize = (0,0), fx = self.resize_scale, fy = self.resize_scale)
+            images = list(map(resizer, images))
+            flow = resizer(flow)
+
         images = np.array(images).transpose(3,0,1,2)
         flow = flow.transpose(2,0,1)
 
@@ -95,11 +105,13 @@ class BaseDataset(Dataset, metaclass = ABCMeta):
 # FlyingChairs
 # ============================================================
 class FlyingChairs(BaseDataset):
-    def __init__(self, dataset_dir, train_or_test = 'train', color = 'rgb', shape = None):
+    def __init__(self, dataset_dir, train_or_test = 'train', color = 'rgb', crop_shape = None, resize_shape = None, resize_scale = None):
         super(FlyingChairs, self).__init__()
         assert train_or_test in ['train', 'test']
         self.color = color
-        self.shape = shape
+        self.crop_shape = crop_shape
+        self.resize_shape = resize_shape
+        self.resize_scale = resize_scale
 
         self.dataset_dir = dataset_dir
         self.train_or_test = train_or_test
@@ -126,11 +138,14 @@ class FlyingThings(BaseDataset):
 class Sintel(BaseDataset):
 
 
-    def __init__(self, dataset_dir, train_or_test, mode = 'final', color = 'rgb', shape = None):
+    def __init__(self, dataset_dir, train_or_test, mode = 'final', color = 'rgb', crop_shape = None, resize_shape = None, resize_scale = None):
         super(Sintel, self).__init__()
         self.mode = mode
         self.color = color
-        self.shape = shape
+        self.crop_shape = crop_shape
+        self.resize_shape = resize_shape
+        self.resize_scale = resize_scale
+
         self.dataset_dir = dataset_dir
         self.train_or_test = train_or_test
         p = Path(dataset_dir) / (train_or_test + '.txt')
@@ -163,7 +178,7 @@ class KITTI(BaseDataset):
 
 
 if __name__ == '__main__':
-    dataset = Sintel('datasets/Sintel', 'train')
+    dataset = Sintel('datasets/Sintel', 'train', crop_shape = (384, 768), resize_scale = 1 / 16)
 
     # for i in range(dataset.__len__()):
     #     images, flow = dataset.__getitem__(i)
