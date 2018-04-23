5aebcb675e97ce5a62743b9b80c164f8afa9ad63
Ranhao Kang <ranhao@momenta.ai>
2018-04-23 22:29:37 +0800

    .

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-B, C, H, W = 8, 128, 48, 48
+B, C, H, W = 8, 192, 48, 48
 S = 4
 
 
@@ -42,3 +42,38 @@ def v5_gpu(src, tgt):
             map_ur = F.pad(tgt[:,:,:-i,j:], (0,j,0,i))
             map_lr = F.pad(tgt[:,:,i:,j:], (0,j,i,0))
             outputs.extend(list(map(f, [map_ul, map_ll, map_ur, map_lr])))
+        
+
+if __name__ == '__main__':
+    import time
+    B = 8
+    Cs = [16, 32, 64, 96, 128, 192]
+    Hs = [192, 96, 48, 24, 12, 6]
+    Ws = [224, 112, 56, 28, 14, 7]
+    print('CPU')
+    times = [0, 0, 0, 0, 0, 0]
+    for i in range(100):
+        for l in range(6):
+            C, H, W = Cs[l], Hs[l], Ws[l]
+            src = Variable(torch.ones((8, C, H, W)))
+            tgt = Variable(torch.ones((8, C, H, W)))
+            t = time.time()
+            v5_cpu(src, tgt)
+            times[l] += time.time() - t
+    for i in range(6):
+        print(f'Lv{i}: {times[i] / 100}')
+
+
+    print('GPU')
+    times = [0, 0, 0, 0, 0, 0]
+    for i in range(100):
+        for l in range(6):
+            C, H, W = Cs[l], Hs[l], Ws[l]
+            src = Variable(torch.ones((8, C, H, W))).cuda()
+            tgt = Variable(torch.ones((8, C, H, W))).cuda()
+            t = time.time()
+            v5_gpu(src, tgt)
+            times[l] += time.time() - t
+    
+    for i in range(6):
+        print(f'Lv{i}: {times[i] / 100}')
