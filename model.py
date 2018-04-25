2443effdf76a0a43b69ab0755218cb0ad7125b0c
Ranhao Kang <ranhao@momenta.ai>
2018-04-25 14:15:52 +0800

    rm test.py; add kitti(TODO)

--- model.py
+++ model.py
@@ -20,6 +20,15 @@ class Net(nn.Module):
             self.cost_volume_layer = CostVolumeLayer(args)
             self.optical_flow_estimators = [OpticalFlowEstimator(args, ch_in + (args.search_range*2+1)**2 + 2) for ch_in in args.lv_chs[::-1]]
         self.context_networks = [ContextNetwork(args, ch_in + 2) for ch_in in args.lv_chs[::-1]]
+
+        self.grid_pyramid = []
+        for l in range(args.num_levels):
+            x = src_features[l]
+            torchHorizontal = torch.linspace(-1.0, 1.0, x.size(3)).view(1, 1, 1, x.size(3)).expand(x.size(0), 1, x.size(2), x.size(3))
+            torchVertical = torch.linspace(-1.0, 1.0, x.size(2)).view(1, 1, x.size(2), 1).expand(x.size(0), 1, x.size(2), x.size(3))
+            grid = torch.cat([torchHorizontal, torchVertical], 1).cuda()
+            grid = Variable(data = grid, volatile = not self.training)
+            self.grid_pyramid.append(grid)
     
     def cuda_(self):
         args = self.args
@@ -47,16 +56,10 @@ class Net(nn.Module):
         # TypeError: Type torch.cuda.FloatTensor doesn't implement stateless method linspace
         # so making grids is done on CPU, and Tensors will be converted to cuda.Tensor and dispatch to GPUs
         # compute grid on each level
-        grid_pyramid = []
-        for l in range(args.num_levels):
-            x = src_features[l]
-            torchHorizontal = torch.linspace(-1.0, 1.0, x.size(3)).view(1, 1, 1, x.size(3)).expand(x.size(0), 1, x.size(2), x.size(3))
-            torchVertical = torch.linspace(-1.0, 1.0, x.size(2)).view(1, 1, x.size(2), 1).expand(x.size(0), 1, x.size(2), x.size(3))
-            grid = torch.cat([torchHorizontal, torchVertical], 1).cuda()
-            grid = Variable(data = grid, volatile = not self.training)
-            grid_pyramid.append(grid)
+        grid_pyramid = self.grid_pyramid
         print(f'Build Grids: {time() - t: .2f}s'); t = time()
         B, C, H, W = src_features[0].size()
+
 
         flow_features, flow_pyramid, flow_refined_pyramid = [], [], []
         for l in range(args.num_levels):
