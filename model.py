b4ea3280a558eb02bbfa5051e3b4e495e9c62dc6
Ranhao Kang <ranhao@momenta.ai>
2018-04-27 18:24:47 +0800

    .

--- model.py
+++ model.py
@@ -71,7 +71,7 @@ class Net(nn.Module):
             # upsample the flow estimated from upper level
 
 
-            if layer_idx > 0: flow = F.upsample(flow, scale_factor = 2, mode = 'bilinear')
+            if layer_idx != args.num_levels - 1: flow = F.upsample(flow, scale_factor = 2, mode = 'bilinear')
             else:
                 device = torch.device(args.device)
                 flow = torch.zeros((B, 2, H, W)).to(device)
