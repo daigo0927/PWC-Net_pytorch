58066f27649410e0da121106e276077298372b5e
Ranhao Kang <ranhao@momenta.ai>
2018-04-27 18:41:54 +0800

    .

--- losses.py
+++ losses.py
@@ -9,6 +9,8 @@ def L1loss(x, y): return (x - y).abs().mean(0).sum()
 def L2loss(x, y): return torch.norm(x - y, p = 2, dim = 1).mean(0).sum()
 
 def training_loss(args, flow_pyramid, flow_gt_pyramid):
+    for i,(j,k) in enumerate(zip(flow_pyramid, flow_gt_pyramid)):
+        print(i, j.size(), k.size())
     return sum(w * L2loss(flow, gt) for w, flow, gt in zip(args.weights, flow_pyramid, flow_gt_pyramid))
     
 def robust_training_loss(args, flow_pyramid, flow_gt_pyramid):
