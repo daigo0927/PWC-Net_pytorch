d56cde2061dc6bb59638324a18778fa04845d335
Ranhao Kang <ranhao@momenta.ai>
2018-04-27 17:11:23 +0800

    .

--- losses.py
+++ losses.py
@@ -5,8 +5,8 @@ import torch.nn.functional as F
 def get_criterion(args):
     return training_loss if args.loss == 'L2' else robust_training_loss
 
-def L1loss(x, y): return (x - y).abs().mean()
-def L2loss(x, y): return torch.norm(x - y, p = 2, dim = 1).mean()
+def L1loss(x, y): return (x - y).abs().mean(0).sum()
+def L2loss(x, y): return torch.norm(x - y, p = 2, dim = 1).mean(0).sum()
 
 def training_loss(args, flow_pyramid, flow_gt_pyramid):
     return sum(w * L2loss(flow, gt) for w, flow, gt in zip(args.weights, flow_pyramid, flow_gt_pyramid))
