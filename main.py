1d9066941954c5f0f567d7d2750720e7ceb49354
Ranhao Kang <ranhao@momenta.ai>
2018-05-02 21:33:00 +0800

    .

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ def train(args):
             # ============================================================
             B = flows[0].size(0)
             for b in range(B):
-                batch = [vis_flow(i) for l in range(len(flows)) for i in np.split(np.array(F.upsample(flows[l][b].unsqueeze(0), 2 ** (6-l)).squeeze(0)).transpose(1,2,0))]
+                batch = [vis_flow(i) for l in range(len(flows)) for i in np.split(np.array(F.upsample(flows[l][b].unsqueeze(0), scale_factor = 2 ** (6-l)).squeeze(0)).transpose(1,2,0))]
                 logger.image_summary(f'flow{num}', np.concatenate(batch + [flow_gt], axis = 1), step)
 
             # for l, flow in enumerate(flows):
