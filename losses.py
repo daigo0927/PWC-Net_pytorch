a3516ab340b82d180acf3a517eb1b4da2aae6cd5
Ranhao Kang <ranhao@momenta.ai>
2018-05-03 19:15:05 +0800

    .

--- losses.py
+++ losses.py
@@ -80,6 +80,7 @@ class MultiScale(nn.Module):
         self.loss_labels = ['MultiScale-'+self.l_type, 'EPE'],
 
     def forward(self, outputs, target):
+        args = self.args
         # target *= self.div_flow
         targets = (avg_pool(target) for avg_pool in self.multiScales)
         loss, epe = 0, 0
